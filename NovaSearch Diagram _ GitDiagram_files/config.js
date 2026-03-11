07569d1e8c63d5f093651c0b0f3024a8a3341377
GinneZhang <32411685+GinneZhang@users.noreply.github.com>
2026-03-11 02:58:15 -0400

    Delete NovaSearch Diagram _ GitDiagram_files directory

--- NovaSearch Diagram _ GitDiagram_files/config.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
-  window._POSTHOG_REMOTE_CONFIG['phc_eFCkNQ44Ek1rS522DObWVHWHHolVQf2HTFwPDFaOIlZ'] = {
-    config: {"analytics": {"endpoint": "/i/v0/e/"}, "autocaptureExceptions": false, "autocapture_opt_out": true, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": true, "web_vitals_allowed_metrics": null}, "conversations": false, "defaultIdentifiedOnly": true, "elementsChainAsString": true, "errorTracking": {"autocaptureExceptions": false, "suppressionRules": []}, "hasFeatureFlags": false, "heatmaps": true, "logs": {"captureConsoleLogs": false}, "productTours": false, "sessionRecording": false, "supportedCompression": ["gzip", "gzip-js"], "surveys": false, "token": "phc_eFCkNQ44Ek1rS522DObWVHWHHolVQf2HTFwPDFaOIlZ"},
-    siteApps: []
-  }
-})();
